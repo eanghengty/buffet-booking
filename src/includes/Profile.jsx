3dfc9d2113754d909097edf6cfd50530c9780b29
eanghengty <hengty174@gmail.com>
2022-04-12 11:42:31 +0700

    finish profile page

--- src/includes/Profile.jsx
+++ src/includes/Profile.jsx
@@ -1,27 +1,36 @@
+import {FaCameraRetro, FaUser, FaCalendarCheck, FaRegHeart} from 'react-icons/fa'
+import './../style/profile.scss'
 const Profile =()=>{
     return(
-        <div>
-            <div>
-            <div>
-                cover
+        <div className='profilediv0'>
+            <div className="profilediv1">
+            <div className="profilediv2">
+                
             </div>
-            <div>
-                profile
-                <p>upload pic</p>
+            
+            <div className="profilediv3">
+            <div className="profilediv4">
+                
+                <FaCameraRetro className="profileicon"/>
+                </div>
+                
             </div>
-            <p>name</p>
-            <div>
-                <div>
-                <img src="" alt="icon1" />
-                <p>My Profile</p>
+            <div className="profilediv6"></div>
+            <div className='profilediv5'><p>Hunger Man</p></div>
+            
+            <div className='profilediv7'>
+                
+                <div className="profilediv8">
+                <FaUser  className="profileicon"/>
+                <div className='profilediv9'><p>My Profile</p></div>
                 </div>
-                <div>
-                    <img src="" alt="icon2" />
-                    <p>My Reservation</p>
+                <div className="profilediv10">
+                    <FaCalendarCheck  className="profileicon"></FaCalendarCheck>
+                    <div className="profilediv11"><p>My Reservation</p></div>
                 </div>
-                <div>
-                    <img src="" alt="icon3" />
-                    <p>My Favourite</p>
+                <div className="profilediv12">
+                    <FaRegHeart  className="profileicon"/>
+                    <div className="profilediv13"><p>My Favourite</p></div>
                 </div>
             </div>
             </div>
