33bd1bf266bd53462d264f0e8a5bb36810af7e49
eanghengty <hengty174@gmail.com>
2022-04-12 18:03:08 +0700

    finish searchpage

--- src/components/Searchpage.jsx
+++ src/components/Searchpage.jsx
@@ -2,40 +2,58 @@ import Footer from "../includes/Footer";
 import Header from "../includes/Header";
 import Promotion from "../includes/Promotion";
 import Searchresult from "../includes/SearchResult";
-
+import './../style/searchpage.scss'
 const Searchpage = ()=>{
     return(
         <div>
             <Header></Header>
-            <div>
-                <input type="text" placeholder="Search . . ." />
-                <p>Search</p>
+            <div className="searchpagediv1">
+            <div className="searchpagediv2">
+                <div className="searchpagediv3"><input type="text" placeholder="Search . . ." /></div>
+                <div className="searchpagediv4"><p>Search</p></div>
             </div>
-            <div>
-                <div>
-                    <div></div>
-                    <p>Filter</p>
-                    <div>
-                        <p>discount</p>
+            <div className="searchpagediv5">
+                
+                <div className="searchpagediv6">
+                    <div className="searchpagediv7">
+                    <div className="searchpageline"><div className="searchpagediv8"></div></div>
+                    <div className="searchpagediv9"><p>Filter</p></div>
+                    </div>
+                    <div className="searchpagediv10">
+                        <div className="searchpagediv11"><p>Discount</p></div>
+                        <form className="searchpageform1">
                         <button>10%</button>
                         <button>20%</button>
                         <button>30%</button>
                         <button>40%</button>
+                        </form>
                     </div>
+                
                 </div>
-                <div>
-                    <div>
+                <div className="searchpagediv12">
+                    <div className="searchpagediv13">
+                        <form className="seachpageform2">
                         <button>Most Booking</button>
                         <button>Lowest Price</button>
                         <button>Highest Price</button>
                         <button>Most Rating</button>
                         <button>Recommended</button>
+                        </form>
                     </div>
+                    <div className='searchpagediv15'><p>Result: 3 Restuarants</p></div>
+                    <div className="searchpagediv14">
+                    
                     <Searchresult></Searchresult>
+                    <Searchresult></Searchresult>
+                    <Searchresult></Searchresult>
+                    </div>
                     
                 </div>
             </div>
+            <div>
+            </div>
             <Promotion></Promotion>
+            </div>
             <Footer></Footer>
         </div>
     )
--- src/includes/SearchResult.jsx
+++ src/includes/SearchResult.jsx
@@ -1,27 +1,40 @@
+import './../style/searchresult.scss'
+import image  from './../images/buffet.png'
+import {FaStar} from 'react-icons/fa'
 const Searchresult = () =>{
     return(
-        <div>
-            <div>
-                        <p>Result: 3 Restuarants</p>
-                        <div>
-                            <div>
-                            <img src="" alt="restuarant" />
+        <div className='searchresultdiv0'>
+            
+            <div className="searchresultdiv1">
+                        
+                        <div className="searchresultdiv3">
+                            <div className='searchresultdiv4'>
+                            <img src={image} alt="restuarant" />
                             </div>
-                            <div>
-                            <h2>Sakura Buffet, Location:1</h2>
-                            <p>type:buffet</p>
-                            <p>Phnom Penh</p>
-                            <div>
-                                <img src="" alt="star" />
-                                <p>(4.0)</p>
+                            <div className='searchresultdiv5'>
+                            <div><h2>Sakura Buffet, Location:1</h2></div>
+                            <div className="searchresultdiv6">
+                            <div><p>type:buffet</p></div>
+                            <div><p>Phnom Penh</p></div>
                             </div>
-                            <p>130 reservations</p>
+                            <div className="searchresultdiv7">
+                            <div className='searchresultdiv8'>
+                                <FaStar className="fastar"></FaStar>
+                                <FaStar className="fastar"></FaStar>
+                                <FaStar className="fastar"></FaStar>
+                                <FaStar className="fastar"></FaStar>
+                                <FaStar className="fastar"></FaStar>
+                                
                             </div>
-                            <div>
-                                <div>
+                            <div className="searchresultdiv9"><p>(4.0)</p></div>
+                            </div>
+                            <div className='searchresultdiv10'><p>130 reservations</p></div>
+                            </div>
+                            <div className='searchresultdiv11'>
+                                <div className='searchresultdiv12'>
                                     <p>promotion</p>
                                 </div>
-                                <div>
+                                <div className='searchresultdiv13'>
                                     <p>Available</p>
                                 </div>
                             </div>
